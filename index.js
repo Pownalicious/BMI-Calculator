dfbcc3a38cace98643cda3c81b4dbbeb16920144
Benjamin <bghassemi@live.nl>
2021-08-05 01:23:45 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", function () {
+    let weightInKG = document.getElementById("weight").value;
+    let heightInM = document.getElementById("height").value;
+    let totalBMI = (weightInKG / (heightInM * heightInM) * 10000)
+    document.getElementById('bmi-output').value = Math.round(totalBMI)
+})
+
+
+
+
+//CODAISSEUR OUTCOME ASSIGNMENT
+// console.log(`
+// weight: ${weightInKG}
+// height: ${heightInM}
+// You BMI is: ${BMI}
+// *******************************************
+// A BMI under 18.5 is considered underweight
+// A BMI above 25 is considered overweight
+// *******************************************
+// `)
+
+// const userBMI = document.getElementById("bmi.index")
+// userBMI.textContent = BMI.toFixed(2);
+
